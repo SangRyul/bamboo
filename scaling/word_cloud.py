b76ead596080fc52e9715c6ca18c256847803779
md98 <makerdark98@naver.com>
2017-05-25 13:26:42 +0900

    rename directory

--- scaling/word_cloud.py
+++ scaling/word_cloud.py
@@ -11,17 +11,17 @@ import webbrowser
 r = lambda: random.randint(0,255)
 color = lambda: (r(), r(), r())
 
-def get_tags(nouns_data, multiplier =5):
+def get_tags(nouns_data:list, multiplier:int = 5):
     r = []
     for word in nouns_data:
-        if int(word[1]) >=10 :
+        if int(word[1]) >=8 :
             r.append({'color':color(), 'tag':word[0], 'size':word[1]*multiplier})
 
     return r
 
 
 
-def draw_cloud(tags, filename, fontname='D2Coding', size= (1920, 1080)):
+def draw_cloud(tags:list, filename:str, fontname:str='D2Coding', size= (1920, 1080)):
     pytagcloud.create_tag_image(tags ,filename,fontname=fontname, size=size, rectangular=True)
     webbrowser.open(filename)
 
